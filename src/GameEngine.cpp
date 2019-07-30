3c13c383e9954869e1ddb848459bf41e9a0dd2ea
louis1001 <luisal1207@gmail.com>
2019-07-30 10:06:58 -0600

    Initial Commit

---
+++ src/GameEngine.cpp
@@ -0,0 +1,9 @@
+//
+//  GameEngine.cpp
+//  TBGE
+//
+//  Created by Luis Gonzalez on 7/30/19.
+//  Copyright © 2019 Luis Gonzalez. All rights reserved.
+//
+
+#include "GameEngine.hpp"
---
+++ src/GameEngine.hpp
@@ -0,0 +1,20 @@
+//
+//  GameEngine.hpp
+//  TBGE
+//
+//  Created by Luis Gonzalez on 7/30/19.
+//  Copyright © 2019 Luis Gonzalez. All rights reserved.
+//
+
+#ifndef GameEngine_hpp
+#define GameEngine_hpp
+
+#include <stdio.h>
+
+namespace TBGE {
+    class GameEngine{
+        GameEngine();
+    };
+}
+
+#endif /* GameEngine_hpp */
